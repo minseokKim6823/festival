bea5ca0f7834852ce5405b2c65baac3f477688e8
minseokKim6823 <alstjr6823@naver.com>
2023-09-18 22:05:43 +0900

    시간 기준으로 DB업데이트(30초)

--- src/main/java/com/admin/festival/service/VisitorService.java
+++ src/main/java/com/admin/festival/service/VisitorService.java
@@ -4,27 +4,32 @@ import com.admin.festival.entity.VisitorEntity;
 import com.admin.festival.repository.VisitorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VisitorService {
     @Autowired
     private VisitorRepository visitorRepository;
-    private Long countset=0L;
+    public long countset=0L;
     @Cacheable("countset")
     public Long getVisitorCount() {
         VisitorEntity visitorEntity = visitorRepository.findById(1L).orElse(new VisitorEntity());
         return visitorEntity.getCount();
     }
 
+
     public Long incrementVisitorCount() {
         VisitorEntity visitorEntity = visitorRepository.findById(1L).orElse(new VisitorEntity());
         countset=countset + 1;
-        if(countset%5==0){
-            visitorEntity.setCount(visitorEntity.getCount() + 5);
-            visitorRepository.save(visitorEntity);
-        }
         return visitorEntity.getCount();
+    }
+    @Scheduled(fixedRate = 30000)//30초 기준
+    public void updateDB() {
+        VisitorEntity visitorEntity = visitorRepository.findById(1L).orElse(new VisitorEntity());
+        visitorEntity.setCount(visitorEntity.getCount() + countset);
+        countset =0;
+        visitorRepository.save(visitorEntity);
     }
 }
 
